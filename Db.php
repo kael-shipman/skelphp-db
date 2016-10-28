155a5c7dc8c72cbcc65f3b12815308f20473dea0
Kael Shipman <kael.shipman@gmail.com>
2016-10-28 11:09:13 -0500

    Updated Db class to be independent of PDO

--- Db.php
+++ Db.php
@@ -1,14 +1,15 @@
 <?php
 namespace Skel;
 
-abstract class Db extends \PDO implements Interfaces\Db {
+abstract class Db implements Interfaces\Db {
   const VERSION = 1;
   const FRAMEWORK_TABLE = 'skel';
   protected $runningVersion;
 
-  public function __construct(string $dsn, string $username=null, string $password=null, array $options=array()) {
-    parent::__construct($dsn, $username, $password, $options);
-    $this->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+  public function __construct(Interfaces\DbConfig $config) {
+    $this->config = $config;
+    $this->db = new \PDO($config->getDbDsn());
+    $this->db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
     $this->initializeDatabase();
     if ($this->runningVersion != static::VERSION) $this->__syncDatabase();
   }
@@ -20,7 +21,7 @@ abstract class Db extends \PDO implements Interfaces\Db {
       $this->runningVersion = $this->querySingle('SELECT "version" FROM "'.static::FRAMEWORK_TABLE.'" ORDER BY "version" DESC LIMIT 1');
     } catch (\Exception $e) {
       // If the query failed, then the database has probably not been initialized yet
-      $this->exec('CREATE TABLE "'.static::FRAMEWORK_TABLE.'" ("version" INTEGER PRIMARY KEY NOT NULL, "install_date" INTEGER NOT NULL)');
+      $this->db->exec('CREATE TABLE "'.static::FRAMEWORK_TABLE.'" ("version" INTEGER PRIMARY KEY NOT NULL, "install_date" INTEGER NOT NULL)');
       $this->__registerVersionChange(0);
     } 
   }
@@ -28,7 +29,7 @@ abstract class Db extends \PDO implements Interfaces\Db {
   public function querySingle(string $query) {
     $values = func_get_args();
     array_shift($values);
-    $stm = $this->prepare($query);
+    $stm = $this->db->prepare($query);
     $stm->execute($values);
     $res = $stm->fetch(\PDO::FETCH_ASSOC);
     if (count($res) == 1) $res = current($res);
@@ -36,7 +37,7 @@ abstract class Db extends \PDO implements Interfaces\Db {
   }
 
   protected function __registerVersionChange(int $newVersion) {
-    $this->exec('INSERT INTO "'.static::FRAMEWORK_TABLE.'" ("install_date", "version") VALUES ('.((new \DateTime())->getTimestamp()).', '.$newVersion.')');
+    $this->db->exec('INSERT INTO "'.static::FRAMEWORK_TABLE.'" ("install_date", "version") VALUES ('.((new \DateTime())->getTimestamp()).', '.$newVersion.')');
     $this->runningVersion = $newVersion;
   }
 
@@ -49,7 +50,7 @@ abstract class Db extends \PDO implements Interfaces\Db {
   }
 
   protected function __syncDatabase() {
-    $this->beginTransaction();
+    $this->db->beginTransaction();
     $v = $this->runningVersion;
     for ($v; $v < static::VERSION; $v++) {
       $this->upgradeDatabase($v+1, $v);
@@ -59,7 +60,7 @@ abstract class Db extends \PDO implements Interfaces\Db {
       $this->downgradeDatabase($v-1, $v);
       $this->__registerVersionChange($v-1);
     }
-    $this->commit();
+    $this->db->commit();
   }
 
   abstract protected function upgradeDatabase(int $targetVersion, int $fromVersion);
