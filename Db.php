1fa903cd9d67d3f91f8f52f39900fa43765fe299
Kael Shipman <kael.shipman@gmail.com>
2016-11-01 15:04:26 -0500

    Starting to refactor Db ideas

--- Db.php
+++ Db.php
@@ -3,50 +3,45 @@ namespace Skel;
 
 abstract class Db implements Interfaces\Db {
   const VERSION = 1;
+  const SCHEMA_NAME = 'skel';
   const FRAMEWORK_TABLE = 'skel';
   protected $runningVersion;
 
   public function __construct(Interfaces\DbConfig $config) {
     $this->config = $config;
-    $this->db = new \PDO($config->getDbDsn());
-    $this->db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+    $this->db = $config->getDbPdo();
     $this->initializeDatabase();
     if ($this->runningVersion != static::VERSION) $this->__syncDatabase();
   }
 
   abstract protected function downgradeDatabase(int $targetVersion, int $fromVersion);
 
+  protected function getContentDir() {
+    if (!$this->contentDir) $this->contentDir = new Uri('file://'.$this->config->getDbContentRoot());
+    return $this->contentDir;
+  }
+
+  public function getString(string $key, string $default='') { return $this->getStrings()[$key] ?: $default; }
+  public function getStrings() {
+    if (!$this->strings) $this->strings = include $this->config->getDbContentRoot().'/strings.php';
+    return $this->strings;
+  }
+
   protected function initializeDatabase() {
     try {
-      $this->runningVersion = $this->querySingle('SELECT "version" FROM "'.static::FRAMEWORK_TABLE.'" ORDER BY "version" DESC LIMIT 1');
+      $stm = $this->db->prepare('SELECT "version" FROM "'.static::FRAMEWORK_TABLE.'" WHERE "schemaName" = ? ORDER BY "version" DESC LIMIT 1');
+      $this->runningVersion = $stm->execute(static::SCHEMA_NAME)->fetchColumn(0); 
+      if ($this->runningVersion === false) $this->__registerVersionChange(0);
     } catch (\Exception $e) {
       // If the query failed, then the database has probably not been initialized yet
-      $this->db->exec('CREATE TABLE "'.static::FRAMEWORK_TABLE.'" ("version" INTEGER PRIMARY KEY NOT NULL, "install_date" INTEGER NOT NULL)');
+      $this->db->exec('CREATE TABLE "'.static::FRAMEWORK_TABLE.'" ("id" INTEGER PRIMARY KEY, "schemaName" STRING NOT NULL, "version" INTEGER NOT NULL, "install_date" INTEGER NOT NULL)');
       $this->__registerVersionChange(0);
     } 
   }
 
-  public function querySingle(string $query) {
-    $values = func_get_args();
-    array_shift($values);
-    $stm = $this->db->prepare($query);
-    $stm->execute($values);
-    $res = $stm->fetch(\PDO::FETCH_ASSOC);
-    if (count($res) == 1) $res = current($res);
-    return $res;
-  }
-
   protected function __registerVersionChange(int $newVersion) {
-    $this->db->exec('INSERT INTO "'.static::FRAMEWORK_TABLE.'" ("install_date", "version") VALUES ('.((new \DateTime())->getTimestamp()).', '.$newVersion.')');
+    $this->db->exec('INSERT INTO "'.static::FRAMEWORK_TABLE.'" ("schemaName", "install_date", "version") VALUES (\''.$this->db->escapeString(static::SCHEMA_NAME).'\', '.((new \DateTime())->getTimestamp()).', '.$newVersion.')');
     $this->runningVersion = $newVersion;
-  }
-
-  public function setValue(string $table, string $key, $newValue) {
-    //TODO: Implement setValue
-  }
-
-  public function save(string $objectName, array $data) {
-    //TODO: Implement save()
   }
 
   protected function __syncDatabase() {
